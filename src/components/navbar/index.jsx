fdc27f07081dc6dd14958b7b1874f19051b96b5f
Daniel <daniel.reis1911@gmail.com>
2024-06-18 16:44:56 -0300

    Iniciando o desenvolvimento da navbar

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./Navbar.module.css"
+
+export function Navbar() {
+    return (
+        <div className={styles.container}>
+            <h1>Aqui vai ficar a navbar</h1>
+        </div>
+    )
+}
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,9 +1,15 @@
 import styles from './Home.module.css';
 
+import { Navbar } from '../../components/navbar';
+
 export function Home() {
   return (
-    <div className={styles.App}>
-      <h1>Hello world</h1>
+    <div>
+      <Navbar />
+
+      <div className={styles.container}>
+        <h1>Hello world</h1>
+      </div>
     </div>
   );
 }
