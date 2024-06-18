7b6ae7e83bd15917f8b688e9da18f29237924654
Daniel <daniel.reis1911@gmail.com>
2024-06-18 14:55:00 -0300

    Corrigindo nomes de arquivos e adicionando a react router dom para a navegação entre screens do projeto.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './global.css';
-import { Home } from './screens/Home';
+import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 );
