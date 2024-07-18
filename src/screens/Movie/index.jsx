136439cf7053bc1838c2dba65daf1850c783e5ad
Daniel <daniel.reis1911@gmail.com>
2024-07-18 17:25:53 -0300

    Desenvolvendo os Cards de publicações para mobile e desktop.

--- src/screens/Movie/index.jsx
+++ src/screens/Movie/index.jsx
@@ -1,10 +1,41 @@
 import styles from './Movie.module.css';
+import { useState, useEffect } from "react";
 
 import { Header } from "../../components/Header"
-import { StandardCards } from '../../components/StandardCard';
+import { FilterModal } from "../../components/FilterModal";
+import { CardMobile } from '../../components/CardMobile'
+import { CardDesktop } from '../../components/CardDesktop';
 import { RiMovie2Fill } from "react-icons/ri";
+import { IoFilter } from "react-icons/io5";
 
 export function Movie() {
+  const [showFilter, setShowFilter] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  const addScreenWidth = () => {
+    setScreenWidth(window.innerWidth);
+  }
+
+  useEffect(() => {
+      window.addEventListener('resize', addScreenWidth);
+
+      return() => {
+          window.removeEventListener('resize', addScreenWidth)
+      }
+  }, [screenWidth]);
+
+  const ScreenRender = () => {
+    if (screenWidth < 1024) {
+        return (
+            <CardMobile />
+        )
+    } else {
+        return (
+             <CardDesktop />
+        )
+    }
+  } 
+
   return (
     <div>
       <Header />
@@ -15,8 +46,29 @@ export function Movie() {
             <RiMovie2Fill className={styles.icon} />
         </div>
 
-        <StandardCards />
+        <div className={styles.container}>
+          <div className={styles.elements}>
+              <div className={styles.title}>
+                  <h3>Mais Recentes</h3>
+                  <span className={styles.underBar}></span>
+              </div>
+
+              <button className={styles.filter} onClick={() => setShowFilter(true)}>
+                  <p>Filtro</p>
+                  <IoFilter className={styles.icon} />
+              </button>
+          </div>
+
+          <div className={styles.cardsContainer}>
+            <ScreenRender />
+          </div>
+        </div>
       </body>
+
+      <FilterModal 
+          showFilter={showFilter} 
+          setShowFilter={() => setShowFilter(!showFilter)} 
+      />
     </div>
   );
 }
