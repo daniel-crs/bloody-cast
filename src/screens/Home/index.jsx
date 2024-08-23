c1297340b03e7fab27bc51abcd6b7ce49db9c466
Daniel <daniel.reis1911@gmail.com>
2024-08-22 22:31:50 -0300

    Criando no back e implementando no front o BloodyCastInfo ao site.

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,4 +1,5 @@
 import styles from './Home.module.css';
+import { useState, useEffect } from "react";
 
 import { Header } from "../../components/Header"
 import { Slider } from '../../components/Slider';
@@ -7,36 +8,38 @@ import { InfoDropdown } from '../../components/InfoDropdown';
 import { Footer } from '../../components/Footer';
 
 export function Home() {
-  const infoOptions = [
-    {title: "Por que criamos o bloodcast", description: "BloodCast é um blog/podcast criado pela nossa equipe  pelo amor que possuímos pelo gênero de terror e pelo  desejo de compartilhar nossos conhecimentos sobre o mesmo com as pessoas."}, 
-    {title: "Quem são os criadores.", description: "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI,"},
-    {title: "Data e horároio de lançamento de novos episódios do nosso podcast.", description: "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI,"}
-  ];
+  const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const url = "http://localhost:1337/api/bloody-cast-infos?populate=*";
+        fetch(url)
+        .then((res) => res.json())
+        .then((post) => {
+            setData(post.data);
+        });
+    }, []);
 
   return (
     <div>
       <Header />
 
       <body className={styles.bodyContainer}>
-        <Slider />
 
-        {/* <FeedBackBanner />
+       <FeedBackBanner />
 
         <div className={styles.cardContainer}>
             <h3>
                 Informações extras
             </h3> 
             <div className={styles.dropdownPosition}>
-              {infoOptions.map(function(data) {
-                return (
-                  <InfoDropdown title={data.title} description={data.description} />
-                )
-              })}
+              {data?.map((post) => (
+                <InfoDropdown title={post.attributes.title} description={post.attributes.description} />
+              ))}
             </div>
-        </div> */}
+        </div>
       </body>
       
-      {/* <Footer /> */}
+     <Footer />
     </div>
   );
 }
